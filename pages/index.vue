52c95071fbf87c3cff5387b1cc415fc12de0bfaf
Felix Gutierrez <lixfe2000g@gmail.com>
2022-02-17 19:34:32 -0500

    Termino video 41

---
+++ pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech mews!</h1>
+    </section>
+    <PostList :posts="loadedPosts" />
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/Posts/PostList'
+export default {
+  components:{
+    PostList
+  },
+  asyncData(context,callback){//callback le indica a la pagina q ya obtubo los datos
+  console.log("asyncDta is executed!!")
+    setTimeout(() => {
+      callback(null,{
+        loadedPosts: [
+          {
+            id: '1', 
+            title: 'First Post', 
+            previewText: 'This is our first post!',
+            thumbnail:'https://ecuador.unir.net/wp-content/uploads/2019/12/mba-tech.jpg'
+          },
+          {
+            id: '2', 
+            title: 'Second Post', 
+            previewText: 'This is our second post!',
+            thumbnail:'https://ecuador.unir.net/wp-content/uploads/2019/12/mba-tech.jpg'
+          }
+        ]
+      });
+    },1500)
+  },
+  /*
+  data(){
+    return{
+      loadedPosts:[
+        
+      ]
+    }
+  },*/
+  created(){//aqui llegan los datos der servidor
+   
+  }
+}
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-image: url('~assets/images/salsa-image.jpg');
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="posts-page">
+        <PostList :posts="loadedPosts" />
+    </div>
+</template>
+
+<script>
+import PostList from '@/components/Posts/PostList'
+export default {
+  components:{
+    PostList
+  },
+  data(){
+    return{
+      loadedPosts:[
+        {
+            id: '1', 
+            title: 'First Post', 
+            previewText: 'This is our first post!',
+            thumbnail:'https://ecuador.unir.net/wp-content/uploads/2019/12/mba-tech.jpg'
+          },
+          {
+            id: '2', 
+            title: 'Second Post', 
+            previewText: 'This is our second post!',
+            thumbnail:'https://ecuador.unir.net/wp-content/uploads/2019/12/mba-tech.jpg'
+          }
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+.posts-page{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
